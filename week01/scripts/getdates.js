6d10c4c2ac15e04969a98771f7cc35df76c32d5d
enoch-bolarinwa <enoch-bolarinwa@>
2024-11-19 14:40:07 +0100

    index.html

--- week01/scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Dynamically set the current year in the footer's first paragraph
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Dynamically set the last modified date in the footer's second paragraph
-document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
